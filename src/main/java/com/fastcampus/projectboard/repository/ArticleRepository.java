1c027528f8d548d6cceae0092fc3d61e19c169c1
eunyoung go <unn3290@gmail.com>
2023-09-10 04:05:02 +0900

    Revert "#1- Querydsl 지원을 이용하여 api 검색옵션 추가"
    This reverts commit 8d467f4afc9c4811ba356c0b530523a2bdec1e3e.

--- src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -2,41 +2,9 @@ package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.domain.Article;
 import com.fastcampus.projectboard.domain.ArticleComment;
-import com.fastcampus.projectboard.domain.QArticle;
-import com.fastcampus.projectboard.domain.QArticleComment;
-import com.querydsl.core.types.dsl.DateTimeExpression;
-import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
-import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends
-        JpaRepository<Article, Long>,
-        QuerydslPredicateExecutor<Article>,
-        QuerydslBinderCustomizer<QArticle> {
-
-
-
-
-    @Override
-    default void customize(QuerydslBindings bindings, QArticle root){
-        bindings.excludeUnlistedProperties(true);
-//          QuerydslPredicateExecutor<Article>에 의해서 모든 필드에 대한 검색 열림,
-//        하지만 우리는 선택적인 필드들만 필터를 하고싶음
-        bindings.including(root.title,root.content, root.hashtag,root.createdAt,root.createdBy);
-        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);//like '$(v)%'
-        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);//like '$(v)%'
-        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);//like '%$(v)%'
-        bindings.bind(root.createdAt).first(DateTimeExpression::eq);//like '%$(v)%'
-        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);//like '%$(v)%'
-
-    }
-
-
-
-
-
+public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
